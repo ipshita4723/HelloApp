33685e63cb1dc8555e6c74142f7d79825d7ea8bd
ipshita4723 <ic4723@srmist.edu.in>
2026-04-02 10:21:53 +0530

    Add files via upload

---
+++ Helloapp_UC7.java
@@ -0,0 +1,15 @@
+public class Helloapp_UC7 {
+    public static void main(String[] args) {
+        
+        if (args.length == 0) {
+           
+            System.out.println("Hello, World!");
+        } else {
+            
+            String names = String.join(", ", args);
+
+           
+            System.out.println("Hello, " + names + "!");
+        }
+    }
+}
